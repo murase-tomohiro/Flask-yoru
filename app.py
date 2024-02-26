01b5029bd4836627a700c0629cd7bd9bc38f88e9
村瀬　友啓 <amtpx256@gmail.com>
2024-02-26 20:06:58 +0900

    タスク追加機能を実装

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+import sqlite3
 app=Flask(__name__)
 
 # @app.route("/")
@@ -13,6 +14,32 @@ app=Flask(__name__)
 def template():
     py_name="むらせ　ともひろ"
     return render_template("index.html",name=py_name)
+
+@app.route("/test")
+def test():
+    py_name="すなばこ太郎"
+    return render_template("base.html",name=py_name)
+
+@app.route("/add")
+def add_get():
+    return render_template("add.html")
+
+@app.route("/add",methods=["POST"])
+def add_post():
+    # 1.入力フォームからデータを取得する
+    task=request.form.get("task")
+    print(task)
+    # 2.データベースに接続する
+    con=sqlite3.connect("myTask.db")
+    # 3.データベースを操作するための準備
+    c=con.cursor()
+    # 4.SQLを実行してDBにデータを送る
+    c.execute("INSERT INTO tasks (id,task) VALUES (null,'{}')".format(task))
+    # 5.データベースを更新（保存）する
+    con.commit()
+    # 6.データベースの接続を終了する
+    c.close()
+    return render_template("add.html")
 
 if __name__=="__main__":
     app.run(debug=True)
